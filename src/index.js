f6ed624fc1b0624f091abff7480029d17367af39
Ghiwa <ghiwa_mortada@hotmail.com>
2023-10-27 12:50:28 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import "@shopify/polaris/build/esm/styles.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
